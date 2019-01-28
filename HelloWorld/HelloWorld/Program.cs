8860881735ac3b0c0d048c1259e5091a56df77f5
Knugi <24708955+knugi0123@users.noreply.github.com>
2019-01-28 22:41:34 +0800

    Add comments

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -4,9 +4,12 @@ namespace HelloWorld
 {
     class Program
     {
+        //Start of the program
         static void Main(string[] args)
         {
+            //Output "Hello, World!" to console(or you can say it's screen)
             Console.Write("Hello, World!");
+            //Use a built-in input method to prevent closing automatically
             Console.ReadLine();
         }
     }
