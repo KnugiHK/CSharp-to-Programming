209307fce04c5cfb63b21efb0b3d22e44b9946c7
Arun Kumar Singh <arunpratap26@yahoo.com>
2019-02-11 15:20:18 +0530

    Updated With Write and WriteLine
    Added the difference between the Write and the WriteLine methods.

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -7,8 +7,13 @@ namespace HelloWorld
         //Start of the program
         static void Main(string[] args)
         {
-            //Output "Hello, World!" to console(or you can say it's screen)
+            //The Write () method outputs one or more values to the screen without a new line character.
             Console.Write("Hello, World!");
+            Console.Write("without a new line ");
+            //The WriteLine() always appends a new line character to the end of the string.
+            //this means any subsequent output will start on a new line.
+            Console.WriteLine("Hello, World!);
+            Console.WriteLine("subsequent output will start on a new line");
             //Use a built-in input method to prevent closing automatically
             Console.ReadLine();
         }
