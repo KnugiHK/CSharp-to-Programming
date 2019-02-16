11f233a34affa72f31e4f61d2ee44b27e7024384
Knugi <24708955+knugi0123@users.noreply.github.com>
2019-02-16 23:15:17 +0800

    Word changed
    Add punctuation and make the first letter to capital letter

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -7,14 +7,14 @@ namespace HelloWorld
         //Start of the program
         static void Main(string[] args)
         {
-            //The Write () method outputs one or more values to the screen without a new line character.
+            //The Write () method outputs one or more values to the screen without a new line character(\n).
             Console.Write("Hello, World!");
-            Console.Write("without a new line ");
-            //The WriteLine() always appends a new line character to the end of the string.
-            //this means any subsequent output will start on a new line.
+            Console.Write("Without a new line.");
+            //The WriteLine() always appends a new line character(\n) to the end of the string.
+            //This means any subsequent output will start on a new line.
             Console.WriteLine("Hello, World!);
-            Console.WriteLine("subsequent output will start on a new line");
-            //Use a built-in input method to prevent closing automatically
+            Console.WriteLine("Subsequent output will start on a new line.");
+            //Use a built-in input method to prevent closing automatically.
             Console.ReadLine();
         }
     }
